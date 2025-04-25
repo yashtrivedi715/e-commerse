728bba88ae67f119823b5f1d92a5998163b0d6d8
yashtrivedi715 <153846943+yashtrivedi715@users.noreply.github.com>
2025-04-26 00:05:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
 
 //configure env
 dotenv.config();
@@ -21,6 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
